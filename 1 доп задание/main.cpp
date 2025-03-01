74d242c3bc8ca4b3ea13fdc8a2f5b98f91722378
RozbOnd <ansaraev@yandex.ru>
2025-03-01 21:58:39 +0300

    added 1 laba

---
+++ 1 доп задание/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <clocale>
+using namespace std;
+
+struct Vector {
+    int size;
+    int* cords;
+
+    Vector(int sz) : size(sz) {
+        cords = new int[sz];
+    }
+
+    ~Vector() {
+        if (cords != NULL){
+            delete[] cords;
+            cords = NULL;
+            size = 0;
+        }
+    }
+};
+
+istream& operator >> (istream &is, Vector &vec){
+    if (vec.cords != NULL) {
+        delete[] vec.cords;
+        vec.size = 0;
+    }
+    for (int i = 0; i < vec.size; i++)
+        is >> vec.cords[i];
+}
+
+ostream& operator << (ostream &os, const Vector &vec){
+    os << "( ";
+    for (int i = 0; i < vec.size; i++)
+        os << vec.cords[i] << " ";
+    os << ")\n";
+}
+
+Vector add(Vector v1, Vector v2) {
+    Vector res(v1.size);
+    for (int i = 0; i < v1.size; i++)
+        res.cords[i] = v1.cords[i] + v2.cords[i];
+    return res;
+}
+
+Vector mul(Vector v, int n) {
+    Vector res(v.size);
+    for (int i = 0; i < v.size; i++)
+        res.cords[i] = v.cords[i] * n;
+    return res;
+}
+
+int scalar_mul(Vector v1, Vector v2) {
+    int res = 0;
+    for (int i = 0; i < v1.size; i++)
+        res += v1.cords[i] * v2.cords[i];
+    return res;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите размер векторов: ";
+    cin >> n;
+
+    Vector a(n), b(n), c(n);
+
+    cout << "Введите элементы вектора a:\n";
+    cin >> a;
+    cout << "Введите элементы вектора b:\n";
+    cin >> b;
+    cout << "Введите элементы вектора c:\n";
+    cin >> c;
+
+    int scal = scalar_mul(b, c);
+    cout << "(b * c)\n" << scal << '\n';
+    Vector cur1 = mul(a, scal);
+    cout << "(b * c) * a\n" << cur1 << '\n';
+    Vector cur2 = mul(a, 3);
+    cout << "3 * a\n" << cur2 << '\n';
+    Vector res = add(cur2, cur1);
+    cout << "3 * a + (b * c) * a\n" << res;
+
+    return 0;
+}
